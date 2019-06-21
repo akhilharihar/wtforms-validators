e4ce65eb2e6adff4a8f726ea291b5b936563d0b4
akhil harihar <akhil01@live.com>
2019-06-21 07:35:52 +0530

    package setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='wtforms-validators',
+    version='0.1.0',
+    author='Akhil Harihar',
+    author_email="akhil01@live.com",
+    description="Validators for wtforms package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akhilharihar/wtforms-validators",
+    packages=['wtforms_validators'],
+    python_requires='>=3.0',
+    install_requires=[
+        'WTForms',
+        'dnspython'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha"
+    ]
+)
